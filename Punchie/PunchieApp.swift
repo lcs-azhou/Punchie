ef6507d9acf28ef37278218c76801794ef64cb6d
Ansheng Zhou <azhou@lcs.on.ca>
2025-02-12 22:56:37 -0500

    Initial Commit

---
+++ Punchie/PunchieApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchieApp.swift
+//  Punchie
+//
+//  Created by Ansheng Zhou on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct PunchieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
